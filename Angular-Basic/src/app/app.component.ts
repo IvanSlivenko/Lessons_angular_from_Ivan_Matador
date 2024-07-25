f453a1fbf9e6e7bbdabc808a2b583f71ef158fc3
IvanSlivenko <ivan8822@ukr.net>
2024-07-25 16:04:13 +0300

    25-07-2024-16-04

--- Angular-Basic/src/app/app.component.ts
+++ Angular-Basic/src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Angular-Basic';
+  isParagraphVisible: boolean = false;
+  isHeedButtonVisible: boolean = false;
+  list: string[] = [
+    'List one',
+    'List two'
+  ];
+  roles: any[]=[
+    {
+      role:'admin',
+      text:'admin'
+    },
+    {
+      role:'user',
+      text:'user'
+    },
+    {
+      role:'customer',
+      text:'Ivan'
+    },
+    {
+      role:'user',
+      text:'user2'
+    }
+  ];
+  
+  
+  show(){
+    console.log('isParagraphVisible', this.isParagraphVisible);
+    
+  }
+  changeIsHeedButtonVisible(){
+    this.isHeedButtonVisible=!this.isHeedButtonVisible
+  }
 }
--- Angular-Basic/src/app/app.module.ts
+++ Angular-Basic/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NewComponent } from './new/new.component';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewComponent
+
+    
   ],
   imports: [
     BrowserModule,
